4799501c072cefe6441d6658d78627061220bb58
ya0igoddess <70133277+ya0igoddess@users.noreply.github.com>
2024-02-26 19:24:23 +0300

    make input impact to ball's physics

--- src/player/player_input.rs
+++ src/player/player_input.rs
@@ -1,25 +1,31 @@
 use bevy::{ecs::system::{Query, Res}, input::{keyboard::KeyCode, ButtonInput}, time::Time, transform::components::Transform};
+use bevy_rapier2d::dynamics::Velocity;
 
 use super::player::InputControllable;
 
 pub fn player_input(
     keys: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
-    mut query: Query<(&mut Transform, &InputControllable)>
+    mut query: Query<(&mut Velocity, &InputControllable)>
 ) {
-    let pos_delta = 100.0 * time.delta_seconds();
-    for (mut transform, _) in query.iter_mut() {
+    let pos_delta = 3000.0 * time.delta_seconds();
+    for (mut vel, _) in query.iter_mut() {
         if keys.pressed(KeyCode::KeyW) {
-            transform.translation.y += pos_delta;
+            vel.linvel.y = pos_delta;
+            vel.linvel.x = 0.;
         }
         if keys.pressed(KeyCode::KeyA) {
-            transform.translation.x -= pos_delta;
+            vel.linvel.x = -pos_delta;
+            vel.linvel.y = 0.;
         }
         if keys.pressed(KeyCode::KeyS) {
-            transform.translation.y -= pos_delta;
+            vel.linvel.y = -pos_delta;
+            vel.linvel.x = 0.;
         }
         if keys.pressed(KeyCode::KeyD) {
-            transform.translation.x += pos_delta;
+            vel.linvel.x = pos_delta;
+            vel.linvel.y = 0.;
         }
+        println!("velocity {:?}", vel)
     }
 }
