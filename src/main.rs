2da34e339bf4dc101b3dceb74004b68da7efdeea
ya0igoddess <70133277+ya0igoddess@users.noreply.github.com>
2024-02-26 19:24:00 +0300

    add bevy_rapier and sample sprite

--- src/main.rs
+++ src/main.rs
@@ -26,24 +26,42 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+    mut rapier_config: ResMut<RapierConfiguration>,
 ) {
+    rapier_config.gravity = Vec2::new(0.0, 0.0);
     commands.spawn(Camera2dBundle::default());
 
+    let tile_texture = asset_server.load("resources/graphics/tile_set_earth1.png");
+
+    for i in 1i16..200i16 {
+        for j in 1i16..100i16 {
+            commands.spawn((
+                SpriteBundle {
+                    texture: tile_texture.clone(),
+                    transform: Transform::from_xyz(f32::try_from(i).unwrap()*16. - 500.,f32::try_from(j).unwrap()*16.- 500.,-1.),
+                    ..default()
+                },
+
+            ));
+        }
+    }
+
     let shapes = [
-        Mesh2dHandle(meshes.add(Circle { radius: 50.0 })),
-        Mesh2dHandle(meshes.add(Ellipse::new(25.0, 50.0))),
-        Mesh2dHandle(meshes.add(Capsule2d::new(25.0, 50.0))),
-        //Mesh2dHandle(meshes.add(Rectangle::new(50.0, 100.0))),
-        Mesh2dHandle(meshes.add(RegularPolygon::new(50.0, 6))),
-        Mesh2dHandle(meshes.add(Triangle2d::new(
+        (Mesh2dHandle(meshes.add(Circle { radius: 50.0 })), Collider::ball(50.0)),
+        (Mesh2dHandle(meshes.add(Ellipse::new(25.0, 50.0))), Collider::capsule_y(25.0, 50.0)),
+        (Mesh2dHandle(meshes.add(Capsule2d::new(25.0, 50.0))), Collider::capsule_y(25.0, 25.0)),
+        (Mesh2dHandle(meshes.add(Rectangle::new(50.0, 100.0))), Collider::cuboid(25.0, 50.0)),
+        (Mesh2dHandle(meshes.add(RegularPolygon::new(50.0, 6))), Collider::ball(50.0)),
+        (Mesh2dHandle(meshes.add(Triangle2d::new(
             Vec2::Y * 50.0,
             Vec2::new(-50.0, -50.0),
             Vec2::new(50.0, -50.0),
-        ))),
+        ))), Collider::triangle(Vec2::Y * 50.0, Vec2::new(-50.0, -50.0), Vec2::new(50.0, -50.0))),
     ];
     let num_shapes = shapes.len();
 
-    for (i, shape) in shapes.into_iter().enumerate() {
+    for (i, (shape, collider)) in shapes.into_iter().enumerate() {
         // Distribute colors evenly across the rainbow.
         let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
 
@@ -57,7 +75,9 @@ fn setup(
                 0.0,
             ),
             ..default()
-        }));
+        },
+        collider)
+        );
     }
     commands.spawn((
         MaterialMesh2dBundle {
@@ -66,6 +86,9 @@ fn setup(
             transform: Transform::from_xyz(0., 0., 0.),
             ..default()
         },
+        Collider::ball(30.0),
+        RigidBody::Dynamic,
+        Velocity::default(),
         InputControllable)
     );
 }
